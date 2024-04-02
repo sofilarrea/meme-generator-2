99fdd366a23c511b6fc05ffaec8753fd93d1c004
sofilarrea <sofialejandra.larrea@gmail.com>
2024-04-02 01:07:44 -0300

    adding the url function

--- script/main.js
+++ script/main.js
@@ -24,6 +24,8 @@ const cuadradoDos = document.getElementById("cuadrado-dos")
 const download = document.getElementById("download")
 const downloadButton = document.getElementById("download-button")
 const heroButton = document.getElementById("hero-section")
+const asideContainer = document.getElementById("aside-container")
+
 
 let modo = "claro"; 
 modoButton.addEventListener("click", () => {
@@ -35,6 +37,7 @@ modoButton.addEventListener("click", () => {
         cuadrado.style.backgroundColor = "fuchsia";
         cuadradoDos.style.backgroundColor= "pink";
         download.style.backgroundColor = "violet";
+        asideContainer.style.backgroundColor = "violet"; // No funciona
         sectionPart.style.color = "black";
         modo = "claro";
     } else  {
@@ -46,7 +49,31 @@ modoButton.addEventListener("click", () => {
         cuadrado.style.backgroundColor = "black";
         cuadradoDos.style.backgroundColor = "grey";
         download.style.backgroundColor = "black";
+        asideContainer.style.backgroundColor = "black"; // No funciona
         sectionPart.style.color = "white";
         modo = "dark";
     }
 });
+
+
+/* Cargar Url Imagen */
+const imgInput = document.getElementById("img_input");
+const squareDos = document.getElementById("cuadrado-dos");
+
+imgInput.addEventListener("keyup", function (event) {
+    console.log("dentro de la funcion")
+    if (event.key === "Enter") {
+        const urlImagen = imgInput.value.trim();
+
+        if (urlImagen) {
+            squareDos.style.backgroundImage = `url('${urlImagen}')`;
+            squareDos.style.backgroundSize = "cover";
+            squareDos.style.backgroundPosition = "center";
+            squareDos.style.backgroundRepeat = "no-repeat";
+            imgInput.value = ""; // Saco la url deel input
+        } else {
+            console.log("No se proporcionó una URL válida para la imagen.");
+        }
+    }
+});
+
